592de30d3eea0a7df94ba78770a5b81b22bd6988
Liza <lizaapostol378@gmail.com>
2024-12-16 12:05:35 +0200

    add localstorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Description from "./components/Description/Description";
 import Options from "./components/Options/Options";
 import Feedback from "./components/Feedback/Feedback";
 import Notification from "./components/Notification/Notification";
 
 function App() {
-  const [feedback, setFeedback] = useState({
-    good: 0,
-    neutral: 0,
-    bad: 0,
+  const [feedback, setFeedback] = useState(() => {
+    const savedData = JSON.parse(localStorage.getItem("feedback"));
+
+    return savedData
+      ? savedData
+      : {
+          good: 0,
+          neutral: 0,
+          bad: 0,
+        };
   });
 
   const updateFeedback = (feedbackType) => {
@@ -25,6 +31,11 @@ function App() {
       bad: 0,
     }));
   };
+
+  useEffect(() => {
+    localStorage.setItem("feedback", JSON.stringify(feedback));
+  }),
+    [feedback];
 
   const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
   const positiveFeedback = Math.round((feedback.good / totalFeedback) * 100);
